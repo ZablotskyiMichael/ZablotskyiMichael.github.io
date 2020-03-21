81a39a3c18929925154f2e8e6c61b7966fdddd57
krotvnore177@gmail.com <2703vbif>
2020-03-22 00:24:00 +0200

    test10

--- testReact.js
+++ testReact.js
@@ -1,2 +1,33 @@
-const domContainer = document.querySelector('#reactDiv');
-ReactDOM.render(e(LikeButton), domContainer);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    );
+};
+
+const AppHeader = () => {
+    return (
+        <h1>My Todo List</h1>
+    );
+};
+
+const SearchPanel = function () {
+    return <input placeholder="search"/>;
+};
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader/>
+            <SearchPanel/>
+            <TodoList/>
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>, document.getElementById('reactDiv'));
